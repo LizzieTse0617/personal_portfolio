26edda0edf7c40cf7cde33462b390e23b082bc26
Lizzie Tse <lizzie.tse617@gmail.com>
2023-05-26 13:04:46 -0400

    fixing deploy image

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from '../src/Home/Home'
-import Contact from '../src/Contact/Contact'
-import About from '../src/About/About'
-import {Link, Route,Routes} from 'react-router-dom'
+
+import Navbar from './Navbar/Navbar';
+import Home from './Home/Home'
+import Contact from './Contact/Contact'
+import About from './About/About'
+import { Route,Routes} from 'react-router-dom'
+import Footer from './Footer/Footer';
+import NotFound from './NotFound/NotFound';
+
+import Products from './Products/Products';
+
+import Skills from './Skills/Skills';
+import Service from './Service/Service';
+import Project from './Project/Project';
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">home</Link>
-          </li>
-          <li>
-            <Link to="/contact">contact</Link>
-          </li>
-          <li>
-            <Link to="/about">about</Link>
-          </li>
-        </ul>
-      </nav>
+       <Navbar />
 
    
-      <Routes>
+       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/services" element={<Service />} />
+        <Route path="/projects" element={<Project />} />
+        <Route path="/product" element={<Products />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-   
+      <Footer />
     </div>
   );
 }
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,116 @@
+import { useState } from 'react';
+import {
+  Box,
+  Flex,
+  Image,
+  Text,
+  Spacer,
+  IconButton,
+  Collapse,
+  useColorModeValue,
+  Stack,
+} from '@chakra-ui/react';
+import { CloseIcon,HamburgerIcon } from '@chakra-ui/icons';
+
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <Box position="sticky" top={0} zIndex={10} pr={4} pl={4}>
+      <Box
+        position="absolute"
+        top={0}
+        left={0}
+        right={0}
+        bottom={0}
+        bg="rgba(0, 0, 0, 0.03)"
+        zIndex={-1}
+        backdropFilter="blur(20px)"
+      />
+      <Flex
+        maxW="1200px"
+        mx="auto"
+        alignItems="center"
+        justifyContent="space-between"
+        bg="transparent"
+        py={4}
+        position="sticky"
+      >
+        <Link to="/">
+          <Image
+            src='../../public/name-logo.svg'
+            alt="Image"
+            height={10}
+          />
+
+<Image src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fHww&w=1000&q=80"/>
+
+
+        </Link>
+
+        
+        <Spacer />
+        <IconButton
+          display={{ base: 'block', md: 'none' }}
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          aria-label="Toggle Menu"
+          color="blue.500"
+          boxSize={10}
+          onClick={handleToggle}
+          p={3}
+        />
+        <Box
+          display={{ base: isOpen ? 'block' : 'none', md: 'flex' }}
+          alignItems="center"
+        >
+          <Stack
+            direction={{ base: 'column', md: 'row' }}
+            spacing={8}
+            fontWeight="bold"
+          >
+
+
+
+            <Link to="/" active>
+              Home
+            </Link>
+            <Link to="/about">About</Link>
+            <Link to="/skills">Skills</Link>
+            <Link to="/services">Services</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/product">Products</Link>
+            <Link to="/contact">Contact</Link>
+          </Stack>
+        </Box>
+      </Flex>
+      <Collapse in={isOpen} animateOpacity color="black">
+        <Flex flexDirection="column" alignItems="flex-end"></Flex>
+      </Collapse>
+    </Box>
+  );
+}
+
+// eslint-disable-next-line react/prop-types
+function NavLink({ href, children, active }) {
+  const linkColor = useColorModeValue('black', 'blue.500');
+  const hoverColor = useColorModeValue('blue.500', 'black');
+  return (
+    <Text
+      fontSize="md"
+      fontWeight="bold"
+      color={active ? linkColor : 'black'}
+      ml={10}
+      _hover={{ color: hoverColor }}
+    >
+      <a href={href}>{children}</a>
+    </Text>
+  );
+}
+
+export default Navbar;
